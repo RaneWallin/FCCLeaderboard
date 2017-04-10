ab46c1a570924c1f8985931562a160b6780bc376
Rane Wallin <rane.wallin@gmail.com>
2017-04-10 15:52:03 -0700

    updating bundle.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ const SiteHeader = () => {
 		<header>
 			<h1>FreeCodeCamp Leaderboard</h1>
 			<h2>Designed and Coded by Rane</h2>
-			<h4>View source on GitHub</h4>
+			<h4>View source on <a href="https://github.com/RaneWallin/FCCLeaderboard/tree/gh-pages">GitHub</a></h4>
 		</header>
 	);
 }
